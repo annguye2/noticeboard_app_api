e91c3ef9bb35650ea45b7612c10ff372280abd1f
524855 <annguyen@new-host-7.home>
2017-05-18 20:53:00 -0400

    changed cors value

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8001'
+    origins 'localhost:2045'
 
     resource '*',
       headers: :any,
-      # methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      methods: [:get, :post :put, :update, :delete, :options, :head]
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      #methods: [:get]
   end
 end
